5baf6fbac3c4e30cf771fa839309a71917b85147
ya-nsh <yansh2017@gmail.com>
2022-01-27 16:51:49 +0530

    refactored to include context

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import Footer from './components/layout/Footer';
 import About from './pages/About';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import GithubContext, { GithubProvider } from './context/github/GithubContext';
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-4 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
-            {/* if none of the above match statement */}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-4 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+              {/* if none of the above match statement */}
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,28 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import GithubContext from '../../context/github/GithubContext';
 import Spinner from '../layout/Spinner';
 import Spinner2 from '../layout/Spinner2';
 import UserItem from './UserItem';
 
 export default function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []);
-
-  const fetchUsers = async () => {
-    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-      }
-    });
-
-    const data = await res.json();
-
-    setUsers(data);
-    setLoading(false);
-  };
 
   return loading ? (
     <Spinner2 />
