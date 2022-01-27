ca9dda8fe89f415127c8b3e7fc01411b0cb741e4
ya-nsh <yansh2017@gmail.com>
2022-01-27 15:59:35 +0530

    displaying the users

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function UserItem({ user }) {
+  return (
+    <div className="card shadow-xl compact side">
+      <div className="flex-row items-center space-x-4 card-body">
+        <div>
+          <div className="avatar">
+            <div className="rounded-full shadow w-14 h-14 mb-8 ring ring-primary-content ring-offset-base-100 ring-offset-2 ">
+              <img src={user?.avatar_url} alt="Profile" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="card-title">{user?.login}</h2>
+          <Link
+            className="text-base-content text-opacity-30"
+            to={`/user/${user?.login}`}
+          >
+            Visit Profile
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import Spinner2 from '../layout/Spinner2';
+import UserItem from './UserItem';
 
 export default function UserResults() {
   const [users, setUsers] = useState([]);
@@ -28,8 +29,9 @@ export default function UserResults() {
   ) : (
     <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 ">
       {users.map(user => (
-        <h3>{user.login}</h3>
+        <UserItem key={user.id} user={user} />
       ))}
+      <UserItem />
     </div>
   );
 }
