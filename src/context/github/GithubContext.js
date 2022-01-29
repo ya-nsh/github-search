0e2a9e90b3489d96dca2a606df5733d5186d0a90
ya-nsh <yansh2017@gmail.com>
2022-01-29 17:15:36 +0530

    replaced usestate with usereducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
@@ -6,8 +7,14 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const [users, setUsers] = useState([]);
+  // const [loading, setLoading] = useState(true);
+
+  const initialState = {
+    users: [],
+    loading: true
+  };
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
     const res = await fetch(`${GITHUB_URL}/users`, {
@@ -18,11 +25,15 @@ export const GithubProvider = ({ children }) => {
 
     const data = await res.json();
 
-    setUsers(data);
-    setLoading(false);
+    // setUsers(data);
+    // setLoading(false);
+
+    dispatch({ type: 'GET_USERS', payload: data }); // payload is the data that we want to pass to the reducer
   };
   return (
-    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+    <GithubContext.Provider
+      value={{ users: state.users, loading: state.loading, fetchUsers }}
+    >
       {children}
     </GithubContext.Provider>
   );
